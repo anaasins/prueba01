0f375515caa48e9671e5fa0206d2d0c41a8fd411
Ana Asins Aleixandre <anasal@floridauniversitaria.es>
2016-09-27 13:09:41 +0200

    programacion

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+//fichero index.php
+//creado por Paco Gomez
+?>
+<html>
+  <head>
+      <title>ACTIVIDAD 03</title>
+      
+  </head>
+  <body>
+  <?php
+        echo "Ana<br>";
+        echo "Asins<br>";
+        print "Aleixandre<br>";
+      ?>
+      </body>
+</html>
